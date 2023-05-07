7c2b201d4778eab40d8bde87024cbb296bd7760d
Justin-mario <79143260+Justin-mario@users.noreply.github.com>
2023-05-07 23:17:12 +0100

    second commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -47,5 +47,59 @@ fun main() {
     cookies.forEach {
         println("menu item: ${it.name}")
     }
+    println()
 
+//    val softBakedMenu = cookies.filter {
+//        it.softBaked
+//    }
+
+    val costLess = cookies.filter {
+        it.price < 1.50
+    }
+
+    costLess.forEach {
+        println("${it.name} - $${it.price}")
+    }
+
+//    softBakedMenu.forEach {
+//        println("${it.name} - $${it.price}")
+//    }
+
+    val fullMenu = cookies.map {
+        "${it.name} - $${it.price}"
+    }
+
+    fullMenu.forEach {
+        println(it)
+    }
+
+    val groupedMenu = cookies.groupBy { it.softBaked }
+
+    val softBakedMenu = groupedMenu[true] ?: emptyList()
+    val crunchyMenu = groupedMenu[false] ?: listOf()
+
+    println("Soft cookies:")
+    softBakedMenu.forEach {
+        println("${it.name} - $${it.price}")
+    }
+    println("Crunchy cookies:")
+    crunchyMenu.forEach {
+        println("${it.name} - $${it.price}")
+    }
+
+    val totalPrice = cookies.fold(0.0) { total, cookie ->
+        total + cookie.price
+    }
+
+    println("Total price: $${totalPrice}")
+
+    val alphabeticalMenu = cookies.sortedBy {
+        it.name
+    }
+
+    println()
+    println("Alphabetical menu:")
+    alphabeticalMenu.forEach {
+        println(it.name)
+    }
 }
